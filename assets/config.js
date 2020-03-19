0aa9464437d128ca65a25a548b041773b0604a73
Leandro <lterrado@gmail.com>
2020-03-19 13:09:41 -0300

    ultima modificación antes del merge

---
+++ assets/config.js
@@ -0,0 +1,8 @@
+const myKeys = {
+    googleKey: 'AIzaSyC83rUSdXBFL44H6Ik4yKUlVTgj-rV9lyU',
+    weatherBitKey: '6cb027be27944c8a9fbcb289000c40d4',
+    openMapKey:'680b6991fcd4d51387f9476c1436c1e0',
+    darkSkyKey:'5318c9053ba17e4d9ea3983acf250c7f'
+}
+
+export default myKeys;
